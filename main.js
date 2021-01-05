51009cee903bf1bce426365f7a41fac3912b1a77
BobCp <1119283622@qq.com>
2021-01-05 11:27:35 +0800

    修改tools命名为utils

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App'
 
 // 请求导入
-import http from 'my_tools/http.js'
+import http from 'my_utils/http.js'
 Vue.prototype.$http = http
 // 跳转传值导入
-import tzcz from 'my_tools/tzcz.js'
+import tzcz from 'my_utils/tzcz.js'
 Vue.prototype.$tzcz = tzcz
 // 自定义NJS导入
-import myNJS from 'my_tools/myNJS.js'
+import myNJS from 'my_utils/myNJS.js'
 Vue.prototype.$myNJS = myNJS
 
 // 问题反馈和添加图片相关的组件
